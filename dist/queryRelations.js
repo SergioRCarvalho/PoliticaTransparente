77945049a7def8bf32802eeeacf2b7bd5e1aa4a3
SergioRCarvalho <sergio_pvz3@hotmail.com>
2022-01-04 20:19:25 +0000

    update

--- dist/queryRelations.js
+++ dist/queryRelations.js
@@ -1,4 +1,5 @@
 'use strict';
+const { stringify } = require('flatted/cjs');
 var __createBinding =
   (this && this.__createBinding) ||
   (Object.create
@@ -38,6 +39,7 @@ var __importStar =
   };
 Object.defineProperty(exports, '__esModule', { value: true });
 const fabric_network_1 = require('fabric-network');
+const FabricCAServices = require('fabric-ca-client');
 const path = __importStar(require('path'));
 const fs = __importStar(require('fs'));
 async function main() {
@@ -63,6 +65,61 @@ async function main() {
       identity: 'Cidadao Admin',
       discovery: { enabled: true, asLocalhost: true },
     };
+
+    // Create a new CA client for interacting with the CA.
+    const caURL =
+      connectionProfile.certificateAuthorities[
+        'cidadaoca-api.127-0-0-1.nip.io:8080'
+      ].url;
+    const ca = new FabricCAServices(caURL);
+
+    const userIdentity = await wallet.get('Cidadao Admin');
+    if (userIdentity) {
+      console.log(
+        'An identity for the user "sss" already exists in the wallet'
+      );
+      // Check to see if we've already enrolled the admin user.
+      const adminIdentity = await wallet.get('Cidadao Admin');
+
+      // build a user object for authenticating with the CA
+      const provider = wallet
+        .getProviderRegistry()
+        .getProvider(adminIdentity.type);
+      const adminUser = await provider.getUserContext(
+        adminIdentity,
+        'Cidadao Admin'
+      );
+
+      const a = adminUser.getSigningIdentity();
+      console.log(stringify(a));
+      // build a user object for authenticating with the CA
+      const secret = await ca.register(
+        {
+          affiliation: '',
+          enrollmentID: 'Cidadao CA',
+          role: '',
+        },
+        adminUser
+      );
+      const enrollment = await ca.enroll({
+        enrollmentID: 'testinguser',
+        enrollmentSecret: secret,
+      });
+      const x509Identity = {
+        credentials: {
+          certificate: enrollment.certificate,
+          privateKey: enrollment.key.toBytes(),
+        },
+        mspId: 'CidadaoMSP',
+        type: 'X.509',
+      };
+      await wallet.import('testinguser', x509Identity);
+      console.log(
+        'Successfully registered and enrolled admin user "testinguser" and imported it into the wallet'
+      );
+      return;
+    }
+
     await gateway.connect(connectionProfile, connectionOptions);
     // Get the network (channel) our contract is deployed to.
     const network = await gateway.getNetwork('mychannel');
