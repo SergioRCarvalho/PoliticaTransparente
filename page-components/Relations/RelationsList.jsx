2ba1d765714e9c84e859b0fa6b619dc9b266c563
SergioRCarvalho <sergio_pvz3@hotmail.com>
2022-01-13 22:10:36 +0000

    pesquisaFunciona

--- page-components/Relations/RelationsList.jsx
+++ page-components/Relations/RelationsList.jsx
@@ -4,33 +4,70 @@ import Wrapper from '@/components/Layout/Wrapper';
 import { Relation } from '@/components/Relation';
 import styles from './RelationsList.module.css';
 import { useRelaPages } from '@/lib/relations';
-import useSearch from 'react-hook-search';
+import React, { useState, useEffect } from 'react';
 
-const items = [];
+// const items = [];
 
 const RelaList = () => {
   const { data } = useRelaPages();
-  const posts = data
+
+  const [searchValue, setSearch] = useState();
+  let posts = data
     ? data.reduce((acc, person) => [...acc, person.resu], [])
     : [];
-  // posts.Record.desc
-  posts.map((relation) => items.push(relation));
 
-  const [filteredItems, search, setSearch] = useSearch(
-    posts,
-    (searchValue, item) => item[1].Record.desc.includes(searchValue)
-  );
-  //(searchValue, item) => item.includes(searchValue)
+  const [filteredResult, setfilteredResult] = useState([]);
+
+  function filterItems(query) {
+    return data[0].resu.filter(function (el) {
+      return el.Record.desc.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    });
+  }
+
+  useEffect(() => {
+    if (searchValue) {
+      onChangeSearch(searchValue);
+    }
+  }, [searchValue]);
+
+  const onChangeSearch = (value) => {
+    let result = [{}];
+
+    result[0] = filterItems(value);
+
+    if (result[0] === undefined || result[0].length === 0) {
+      setfilteredResult([]);
+    } else {
+      setfilteredResult(result);
+    }
+  };
+
   return (
     <div className={styles.root}>
-      <input value={search} onChange={setSearch} />
+      <input
+        id="search"
+        onChange={(e) => {
+          e.preventDefault();
+          setSearch(e.target.value);
+          if (e.target.value === '') {
+            setfilteredResult([]);
+          }
+        }}
+        value={searchValue || ''}
+      />
       <Spacer axis="vertical" size={1} />
       <Wrapper>
-        {filteredItems.map((relation) => (
-          <div key={relation.Key} className={styles.wrap}>
-            <Relation className={styles.post} relation={relation} />
-          </div>
-        ))}
+        {filteredResult.length === 0
+          ? posts.map((relation) => (
+              <div key={relation.Key} className={styles.wrap}>
+                <Relation className={styles.post} relation={relation} />
+              </div>
+            ))
+          : filteredResult.map((relation) => (
+              <div key={relation.Key} className={styles.wrap}>
+                <Relation className={styles.post} relation={relation} />
+              </div>
+            ))}
       </Wrapper>
     </div>
   );
