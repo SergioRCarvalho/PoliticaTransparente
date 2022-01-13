c676e27b0fb11766ab3e2d5cb1b776473dfba288
SergioRCarvalho <sergio_pvz3@hotmail.com>
2022-01-13 00:15:37 +0000

    update

--- page-components/Relations/RelationsList.jsx
+++ page-components/Relations/RelationsList.jsx
@@ -4,18 +4,29 @@ import Wrapper from '@/components/Layout/Wrapper';
 import { Relation } from '@/components/Relation';
 import styles from './RelationsList.module.css';
 import { useRelaPages } from '@/lib/relations';
+import useSearch from 'react-hook-search';
+
+const items = [];
 
 const RelaList = () => {
   const { data } = useRelaPages();
   const posts = data
     ? data.reduce((acc, person) => [...acc, person.resu], [])
     : [];
+  // posts.Record.desc
+  posts.map((relation) => items.push(relation));
 
+  const [filteredItems, search, setSearch] = useSearch(
+    posts,
+    (searchValue, item) => item[1].Record.desc.includes(searchValue)
+  );
+  //(searchValue, item) => item.includes(searchValue)
   return (
     <div className={styles.root}>
+      <input value={search} onChange={setSearch} />
       <Spacer axis="vertical" size={1} />
       <Wrapper>
-        {posts.map((relation) => (
+        {filteredItems.map((relation) => (
           <div key={relation.Key} className={styles.wrap}>
             <Relation className={styles.post} relation={relation} />
           </div>
